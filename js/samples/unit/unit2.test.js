6946273f6e1e6c2e59d6afe83f9c292cfc196fea
ninotna <antonin@dev87.fr>
2024-10-31 13:48:15 +0100

    Revert "Ajout d'un exemple de commit pour tester travis-ci"
    This reverts commit 2378dfdb578aa40dd07b07d9ceadf7bab9fffe2b.

--- js/samples/unit/unit2.test.js
+++
@@ -1,6 +0,0 @@
-import { getAge } from "./unit2";
-
-describe("getage unit Test Suites", () =>
-  it('should render "Vous avez actuellement 31 ans"', () => {
-    expect(getAge(31)).toBe("Vous avez actuellement 31 ans");
-  }));
